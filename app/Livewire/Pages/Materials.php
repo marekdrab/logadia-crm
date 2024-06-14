e9bcccfc1559353b57296dc0463fd3f6e90f9a24
marekdrab <marek.drablp@gmail.com>
2024-06-14 13:50:47 +0200

    material model and page added

---
+++ app/Livewire/Pages/Materials.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Livewire\Pages;
+use Livewire\Component;
+
+class Materials extends Component
+{
+    public function render()
+    {
+        return view('materials.index');
+    }
+}
